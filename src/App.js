a740d48628820c413bec88dcb886129a5f7c75c4
ripal-designer <55450429+ripal-designer@users.noreply.github.com>
2022-05-02 11:33:40 -0400

    ain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import ArrayRemoveItem from './components/ArrayRemoveItem';
+import Test from './components/Test';
+import AddItemArray from './components/AddItemArray';
+import StringReverse from './components/StringReverse';
+import Glist from './components/Glist';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <div className='row'>
+      <div class="d-flex align-self-center justify-content-center mb-3" style={{height: "100vh",
+    alignItems: "center"}}>
+          <Glist /> </div>
+    
+     </div></div>
     </div>
   );
 }
